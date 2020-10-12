faa01e6537b1bbbaddd1efe5bade552b00414766
anyholic <anyholic@me.com>
2020-10-12 20:30:26 +0900

    Part2

---
+++ Assets/Scripts/Solitaire.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Solitaire : MonoBehaviour
+{
+    public Sprite[] cardFaces;
+    public GameObject cardPrefab;
+    public GameObject[] bottomPos;
+    public GameObject[] topPos;
+
+    public static string[] suite = new string[] { "C", "D", "H", "S" };
+    public static string[] values = new string[] {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+    public List<string>[] bottoms;
+    public List<string>[] tops;
+
+    private List<string> bottom0 = new List<string>();
+    private List<string> bottom1 = new List<string>();
+    private List<string> bottom2 = new List<string>();
+    private List<string> bottom3 = new List<string>();
+    private List<string> bottom4 = new List<string>();
+    private List<string> bottom5 = new List<string>();
+    private List<string> bottom6 = new List<string>();
+
+    public List<string> deck;
+
+    void Start()
+    {
+        bottoms = new List<string>[] { bottom0, bottom1, bottom2, bottom3, bottom4, bottom5, bottom6 };
+        PlayCards();
+    }
+
+    public void PlayCards()
+    {
+        deck = GenerateDeck();
+        Shuffle(deck);
+        SolitaireSort();
+        StartCoroutine(SolitaireDeal());
+    } 
+
+    // 덱 생성
+    public static List<string> GenerateDeck()
+    {
+        List<string> newDeck = new List<string>();
+        foreach (string s in suite)
+        {
+            foreach (string v in values)
+            {
+                newDeck.Add(s + v);
+            }
+        }
+         
+        return newDeck;
+    }
+
+    // 덱 카드 셔플
+    void Shuffle<T>(List<T> list)
+    {
+        System.Random random = new System.Random();
+        int n = list.Count;
+        while(n>1)
+        {
+            int k = random.Next(n);
+            n--;
+            T temp = list[k];
+            list[k] = list[n];
+            list[n] = temp;
+        }
+    }
+
+    // 카드 52장에 대한 게임오브젝트 생성
+    IEnumerator SolitaireDeal()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            float yOffset = 0;
+            float zOffset = 0.03f;
+            foreach (string card in bottoms[i])
+            {
+                yield return new WaitForSeconds(0.01f);
+                GameObject newCard = Instantiate(cardPrefab, new Vector3(bottomPos[i].transform.position.x, bottomPos[i].transform.position.y - yOffset, bottomPos[i].transform.position.z - zOffset), Quaternion.identity, bottomPos[i].transform);
+                newCard.name = card;
+                if(card == bottoms[i][bottoms[i].Count-1])
+                {
+                    newCard.GetComponent<Selectable>().faceUp = true;
+                }
+                
+                yOffset += 0.3f;
+                zOffset += 0.03f;
+            }
+        }
+    }
+
+    void SolitaireSort()
+    {
+        for (int i=0;i<7;i++)
+        {
+            for(int j=i;j<7;j++)
+            {
+                bottoms[j].Add(deck.Last<string>());
+                deck.RemoveAt(deck.Count - 1);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/UpdataSprite.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdataSprite : MonoBehaviour
+{
+    public Sprite cardFace;
+    public Sprite cardBack;
+    private SpriteRenderer spriteRenderer;
+    private Selectable selectable;
+    private Solitaire solitaire;
+
+    void Start()
+    {
+        List<string> deck = Solitaire.GenerateDeck();
+        solitaire = FindObjectOfType<Solitaire>();
+
+        int i = 0;
+        foreach (string card in deck)
+        {
+            if(this.name == card)
+            {
+                cardFace = solitaire.cardFaces[i];
+                break;
+            }
+            i++;
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        selectable = GetComponent<Selectable>();
+    }
+
+    void Update()
+    {
+        if (selectable.faceUp == true)
+        {
+            spriteRenderer.sprite = cardFace;
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+        }
+    }
+}
